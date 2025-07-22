50a89fee31b5f852f258170cc2da04a13e8aa41f
mehulcs <mehul.becs@gmail.com>
2025-07-22 14:17:12 +0530

    enable caching in network volume

--- handler.py
+++ handler.py
@@ -1,19 +1,28 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import runpod
 import torch
+import os
+
 
 def generate_code(prompt: str) -> str:
-    tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct", trust_remote_code=True)
-    model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct", trust_remote_code=True, torch_dtype=torch.bfloat16).cuda()
-    
+    cache_dir = "/runpod-volume/cache"
+    os.makedirs(cache_dir, exist_ok=True)
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        "deepseek-ai/deepseek-coder-6.7b-instruct", trust_remote_code=True, cache_dir=cache_dir)
+    model = AutoModelForCausalLM.from_pretrained(
+        "deepseek-ai/deepseek-coder-6.7b-instruct", trust_remote_code=True, torch_dtype=torch.bfloat16, cache_dir=cache_dir).cuda()
+
     input_ids = tokenizer(prompt, return_tensors="pt").to(model.device)
     output = model.generate(**input_ids, max_length=512)
     return tokenizer.decode(output[0], skip_special_tokens=True)[len(prompt):]
 
+
 def handler(event):
     # Extract input data from the request
     input_data = event["input"]
-    
+
     return {"result": generate_code(input_data['prompt'])}
+
 
 runpod.serverless.start({"handler": handler})
